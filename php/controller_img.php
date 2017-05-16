f9522174fe0b77437d7b6a4a19b53aca5bbf09a6
Unknown <sebascoscia@gmail.com>
2017-05-16 18:23:58 -0300

    .-

--- php/controller_img.php
+++
@@ -1,7 +0,0 @@
-<?php session_start(); ?>
-<?php include 'controller-registro-php.php'; ?>
-<?php
-$path = dirname(__FILE__) . '/../img';
-$erroresImg = guardarImagen('foto_usuario', $path);
-header('Location: ../home.php');
- ?>
--- registro.php
+++ registro.php
@@ -193,43 +193,6 @@ include ("php/controller-registro-php.php");
     		</div>
     	</div>
 
-    <div class="container_footer">
-      <footer>
-
-        <div class="footer_top">
-          <div class="columna1">
-            <label class="label_footer_col">SOBRE LA COMPAÑÍA</label>
-            <ul>
-              <li><a href="#">Sobre autopool</a></li>
-              <li><a href="#">Empleo</a></li>
-              <li><a href="#">Contacto</a></li>
-            </ul>
-          </div>
-          <div class="columna2">
-            <label class="label_footer_col">VIAJAR CON AUTOPOOL</label>
-            <ul>
-              <li><a href="#">Confianza y Seguridad</a></li>
-              <li><a href="#">Calificación de los Usuarios</a></li>
-              <li><a href="faq.html">Preguntas Frecuentes</a></li>
-              <li><a href="#">Confianza y Seguridad</a></li>
-            </ul>
-          </div>
-          <div class="columna3">
-            <label class="label_footer_col">INFORMACIÓN LEGAL</label>
-            <ul>
-              <li><a href="#">Políticas de Uso</a></li>
-              <li><a href="#">Aviso de Privacidad</a></li>
-              <li><a href="#">Políticas de Cookies</a></li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="footer_bottom">
-            <p>autopool - Todos los derechos reservados.</p>
-
-        </div>
-
-      </footer>
-    </div>
+<?php include 'footer.html' ;?>
   </body>
 </html>
